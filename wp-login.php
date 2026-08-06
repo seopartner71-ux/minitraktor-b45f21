1316bfb4e3d8b3f770eb5da36439e43b11b9a183
seopartner71-ux <seo.partner71@gmail.com>
2026-08-06 09:08:27 +0300

    Update generated site for minitraktor-b45f21-pi.vercel.app

--- wp-login.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html><head>
-<meta charset="UTF-8">
-<meta http-equiv="refresh" content="0; url=https://minitraktor-b45f21-pi.vercel.app/">
-<link rel="canonical" href="https://minitraktor-b45f21-pi.vercel.app/">
-<title>Redirecting…</title>
-</head>
-<body>Redirecting to <a href="https://minitraktor-b45f21-pi.vercel.app/">https://minitraktor-b45f21-pi.vercel.app/</a>.</body>
-</html>
